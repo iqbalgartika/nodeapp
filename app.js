a83d56a5ad2b865ef31485970efed2f755eda345
iqbal.gartika@gmail.com <iqbal123>
2020-04-07 11:39:18 +0700

    template engine

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
-const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,7 +17,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
